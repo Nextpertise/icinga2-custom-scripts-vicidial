49291091411dd463d24f788da598bb37b0ced019
Joachim <joagamed@gmail.com>
2019-03-11 16:56:45 +0100

    Update check_asterisk_peers.sh

--- scripts/check_asterisk_peers.sh
+++ scripts/check_asterisk_peers.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 
+asteriskspeers=$(/usr/sbin/asterisk -rvx 'sip show peers' | grep -v cmcom | grep -v 'Name/username' | grep '/' | grep 'OK' | wc -l)
+
+echo "Aantal active peers: ${asteriskspeers}"
