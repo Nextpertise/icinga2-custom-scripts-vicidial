894df6a7642fe902d8aec49a718e5b0a27101183
Joachim <joagamed@gmail.com>
2019-03-22 10:33:01 +0100

    Update check_swap.sh

--- scripts/check_swap.sh
+++ scripts/check_swap.sh
@@ -1,10 +1,23 @@
-#!/bin/bash                                                                                                                          
-                                                                                                                                     
-swapusage=$(free | grep Swap | awk '{print $3}')                                                                                     
-if [ "${swapusage}" == "0" ]; then                                                                                                   
-        echo "OK - No swap used"                                                                                                     
-        exit 0                                                                                                                       
-fi                                                                                                                                   
-                                                                                                                                     
-echo "NOK - ${swapusage}k on swap used"                                                                                              
-exit 2        
+#!/bin/bash
+
+warning=$1
+critical=$2
+
+if [ -z "${warning}" ]; then
+	warning=0
+fi
+if [ -z "${critical}" ]; then
+	critical=0
+fi
+
+swapusage=$(free | grep Swap | awk '{print $3}')
+if [ "${swapusage}" -gt "${critical}" ]; then
+        echo "Critical - ${swapusage}k on swap used"
+        exit 2
+elif [ "${swapusage}" -gt "${warning}" ]; then
+        echo "Warning - ${swapusage}k on swap used"
+        exit 1
+fi
+
+echo "OK - ${swapusage} on swap used"
+exit 0
