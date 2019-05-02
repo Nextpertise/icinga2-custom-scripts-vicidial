610d08c5bdc5e09ef081fbd799dedabcbec9b4cd
Joachim <joagamed@gmail.com>
2019-05-02 11:56:08 +0200

    Update check_peers_unreachable.sh

--- scripts/check_peers_unreachable.sh
+++ scripts/check_peers_unreachable.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-unreachable_check=$(/usr/sbin/asterisk -rvx 'sip show peers' | grep 'UNREACHABLE' | wc -l)
+unreachable_check=$(/usr/sbin/asterisk -rvx 'sip show peers' | egrep -v '[2-9][0-9][0-9]/' | grep 'UNREACHABLE' | wc -l)
+
 if [ "${unreachable_check}" -gt "0" ]; then
         echo "Status: NOK, total peers unreachable: ${unreachable_check}"
         exit 2
